2709971272538dd0ddd21a77a3fefe69256e7980
aidant11 <aidant10@nycstudents.net>
2025-02-04 21:53:54 +0000

    123

--- Main.java
+++ Main.java
@@ -1,13 +1,33 @@
-public class Main {
-
-	public static void main(String[] args)
+public class Main
+{
+    public static void main(String args[])
     {
-        boolean [][] schedule = new boolean[8][60];
+        boolean[][] schedule = new boolean [8][60];
         AppointmentBook a = new AppointmentBook(schedule);
-        for(int i=10; i<15; i++) schedule [1][i] = true;
-        for(int i=30; i<45; i++) schedule [1][i] = true;
-        for(int i=50; i<60; i++) schedule [1][i] = true;
+        for( int i = 10; i < 15; i++) schedule[1][i] = true;
+        for( int i = 30; i < 45; i++) schedule[1][i] = true;
+        for( int i = 50; i < 60; i++) schedule[1][i] = true;
+        for (int i = 49; i < 60; i++) schedule[2][i] = true;
+        for (int i = 29; i < 45; i++) schedule[2][i] = true;
+        for (int i = 25; i < 30; i++) schedule[1][i] = true;
+        for (int i = 0; i < 15; i++) schedule[2][i] = true;
+        for (int i = 41; i < 60; i++) schedule[2][i] = true;
+        for (int i = 5; i < 30; i++) schedule[3][i] = true;
+        for (int i = 44; i < 60; i++) schedule[3][i] = true;
         a.printPeriod(2);
-        a.findFreeBlock(2,15);
-	}
+        a.findFreeBlock(2, 15);
+        System.out.println(a);
+        
+       AppointmentBook b = new AppointmentBook(schedule);
+       int period = 2; 
+       while (period < 5)
+       {
+        System.out.println("Period" + period);
+        b.printPeriod(period);
+        period++;
+       }
+       b.printPeriod(4);
+       System.out.println(b.makeAppointment(2, 4, 22));
+       b.printPeriod(4);
+    }
 }
